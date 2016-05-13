3ce0348ea79ffa268a54e1c79e52a3112d0e49bb
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-05-13 17:36:29 -0400

    set up scroll and modal for action buttons

--- app/assets/javascripts/action-buttons.js
+++ app/assets/javascripts/action-buttons.js
@@ -3,12 +3,12 @@ $(document).on('turbolinks:load', function(){
 
 
 		if(event.target.id === 'projects-scroll'){
-			alert("do stuff with projects");
+			
 			$('html,body').animate({
 				scrollTop: $('.parallax-static-content').offset().top
 			});
 		} else if (event.target.id === 'music-scroll') {
-			alert("do stuff with music");
+
 			$('#modal-1').prop('checked', true);
 		}
 
