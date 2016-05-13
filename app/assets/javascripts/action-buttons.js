83c5d496e1832c07d0d26897b617eadcb7138220
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-05-13 14:53:23 -0400

    add scroll animation for projects on home page

---
+++ app/assets/javascripts/action-buttons.js
@@ -0,0 +1,15 @@
+$(document).on('turbolinks:load', function(){
+	$('.action-button-container').click(function(event){
+
+
+		if(event.target.id === 'projects-scroll'){
+			alert("do stuff with projects");
+			$('html,body').animate({
+				scrollTop: $('.parallax-static-content').offset().top
+			});
+		} else if (event.target.id === 'music-scroll') {
+			alert("do stuff with music");
+		}
+
+	})
+})
