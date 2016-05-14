5657c172d4957da15e33fe4c1ab6ae12d9a1aee3
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-05-14 04:41:25 -0400

    fix action button bug

--- app/assets/javascripts/action-buttons.js
+++ app/assets/javascripts/action-buttons.js
@@ -1,16 +1,14 @@
 $(document).on('turbolinks:load', function(){
-	$('.action-button-container').click(function(event){
-
-
+	$('.action-button-container, .cover-action-button-container').click(function(event){
 		if(event.target.id === 'projects-scroll'){
-			
+
 			$('html,body').animate({
 				scrollTop: $('.parallax-static-content').offset().top
 			});
 		} else if (event.target.id === 'music-scroll') {
-
+			alert(event.target.id);
 			$('#modal-1').prop('checked', true);
+			// $('#modal-1').attr('checked', 'checked');
 		}
-
 	})
 })
