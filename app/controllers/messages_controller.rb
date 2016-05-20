a331317b692f4afe79aec735f0ea200fc21e7e56
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-05-20 00:26:27 -0400

    fill in create action for messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,29 @@
 class MessagesController < ApplicationController
-  def new
-  end
+
 
   def create
+	  @message = Message.create(message_params)
+	  if @message.save
+		  flash.now[:notice] = 'Thank you for your message - I will get back to you shortly!'
+		  respond_to do |format|
+			  format.html{}
+			  format.js{}
+		  end
+	  else
+		  respond_to do |format|
+			  format.html{}
+			  format.js{}
+		  end
+	  end
   end
+
+  private
+
+  def message_params
+	  params.require(:message).permit(:first_name, :last_name, :email, :phone, :content)
+  end
+
+
+
+
 end
