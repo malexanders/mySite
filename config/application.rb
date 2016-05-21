fa3787a4b6f889174e318b9f72881d1840c25808
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-05-20 20:01:33 -0400

    set gmail username and password to ENV variables

--- config/application.rb
+++ config/application.rb
@@ -36,8 +36,8 @@ module MySite
 	  address: "smtp.gmail.com",
 	  port: 587,
 	  domain: "gmail.com",
-	  user_name: "matthewalexander108", #Save this as environment variable
-	  password: "catfood9", #Save this as environment variable
+	  user_name: ENV["GMAIL_USERNAME"],
+	  password: ENV["GMAIL_PASSWORD"],
 	  authentication: :plain,
 	  enable_starttls_auto: true
 	}
