aef0f570988fbdc6803912b89d45e8f4a91cb2a4
Matthew Alexander Smith <matthewalexander108@gmail.com>
2016-05-20 21:08:24 -0400

    experimenting with production.rb config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,8 +62,8 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-#   config.action_mailer.raise_delivery_errors = true
-#   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
 #   ActionMailer::Base.smtp_settings = {
 #   :address => 'smtp.gmail.com',
 #   :port => 587,
@@ -73,19 +73,19 @@ Rails.application.configure do
 #   :enable_starttls_auto => true,
 #
 # }
-	config.action_mailer.default_url_options = { :host => 'mySite.herokuapp.com' }
+	# config.action_mailer.default_url_options = { :host => 'mySite.herokuapp.com' }
 	config.action_mailer.perform_deliveries = true
 
-	config.action_mailer.default :charset => "utf-8"
-	config.action_mailer.smtp_settings = {
-	address: "smtp.gmail.com",
-	port: 587,
-	domain: "myapp.herokuapp.com",
-	authentication: "plain",
-	enable_starttls_auto: true,
-	user_name: ENV["GMAIL_USERNAME"],
-	password: ENV["GMAIL_PASSWORD"]
-	}
+	# config.action_mailer.default :charset => "utf-8"
+	# config.action_mailer.smtp_settings = {
+	# address: "smtp.gmail.com",
+	# port: 587,
+	# domain: "myapp.herokuapp.com",
+	# authentication: "plain",
+	# enable_starttls_auto: true,
+	# user_name: ENV["GMAIL_USERNAME"],
+	# password: ENV["GMAIL_PASSWORD"]
+	# }
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
